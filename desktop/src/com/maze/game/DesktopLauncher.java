7ee391a44974180b012417c99ad38eb2fa486c20
Hanno Witzleb <hanno.witzleb@sandstorm.de>
2022-05-10 14:08:18 +0200

    initial commit

---
+++ desktop/src/com/maze/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.maze.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.maze.game.MazeGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Maze Game");
+		new Lwjgl3Application(new MazeGame(), config);
+	}
+}
