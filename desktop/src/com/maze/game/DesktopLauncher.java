586382a04eb9cbd290904b2ebaf9843117772341
Laurenzz02 <laurenz.oppelt@gmail.com>
2022-05-16 15:18:06 +0200

    Waterdrop Game

--- desktop/src/com/maze/game/DesktopLauncher.java
+++ desktop/src/com/maze/game/DesktopLauncher.java
@@ -6,10 +6,15 @@ import com.maze.game.MazeGame;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
+	public final static int SCREEN_WIDTH = 800;
+	public final static int SCREEN_HEIGHT = 480;
+
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("Maze Game");
-		new Lwjgl3Application(new MazeGame(), config);
+		config.setWindowedMode(SCREEN_WIDTH, SCREEN_HEIGHT);
+		config.useVsync(true);
+		config.setTitle("Drop Game");
+		new Lwjgl3Application(new Drop(), config);
 	}
 }
