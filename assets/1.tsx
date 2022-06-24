2351b8b71a0abfcda1a92d650dfc1ae6f661ece3
Jörn Drechsler <66793144+pytrxl@users.noreply.github.com>
2022-06-24 13:46:06 +0200

    add tile set v1 prototype & map prototype

---
+++ assets/1.tsx
@@ -0,0 +1,180 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="1" tilewidth="32" tileheight="32" tilecount="32" columns="1">
+ <image source="1.png" width="32" height="1024"/>
+ <tile id="1">
+  <properties>
+   <property name="door_direction" value="E"/>
+   <property name="door_status" type="int" value="1"/>
+   <property name="door_type" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="door_direction" value="E"/>
+   <property name="door_status" type="int" value="1"/>
+   <property name="door_type" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="door_direction" value="E"/>
+   <property name="door_status" type="int" value="1"/>
+   <property name="door_type" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="door_direction" value="N"/>
+   <property name="door_status" type="int" value="1"/>
+   <property name="door_type" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="door_direction" value="N"/>
+   <property name="door_status" type="int" value="1"/>
+   <property name="door_type" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="door_direction" value="N"/>
+   <property name="door_status" type="int" value="1"/>
+   <property name="door_type" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="door_direction" value="S"/>
+   <property name="door_status" type="int" value="1"/>
+   <property name="door_type" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="door_direction" value="S"/>
+   <property name="door_status" type="int" value="1"/>
+   <property name="door_type" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="door_direction" value="S"/>
+   <property name="door_status" type="int" value="1"/>
+   <property name="door_type" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="door_direction" value="W"/>
+   <property name="door_status" type="int" value="1"/>
+   <property name="door_type" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="door_direction" value="W"/>
+   <property name="door_status" type="int" value="1"/>
+   <property name="door_type" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="door_direction" value="W"/>
+   <property name="door_status" type="int" value="1"/>
+   <property name="door_type" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="trap" value=""/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="victory" value=""/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="trap" value=""/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="victory" value=""/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="key_status" type="int" value="1"/>
+   <property name="key_type" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="key_status" type="int" value="1"/>
+   <property name="key_type" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="key_status" type="int" value="1"/>
+   <property name="key_type" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="wall_collision" value=""/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="wall_collision" value=""/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="wall_collision" value=""/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="wall_collision" value=""/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="wall_collision" value=""/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="wall_collision" value=""/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="wall_collision" value=""/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="wall_collision" value=""/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="wall_collision" value=""/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="wall_collision" value=""/>
+  </properties>
+ </tile>
+</tileset>
