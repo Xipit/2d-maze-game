1c43c7da8bfa3cf0fe2c2d338211c4cb1213a986
Laurenz Oppelt <laurenz.oppelt@gmail.com>
2022-07-02 16:13:51 +0200

    Implements the class for TheSectretChambers level

---
+++ core/src/com/maze/game/levels/TheSecretChambers.java
@@ -0,0 +1,14 @@
+package com.maze.game.levels;
+
+/**
+ * <h1>The Secret Chambers</h1>
+ *
+ * @author  Laurenz Oppelt
+ */
+
+
+public class TheSecretChambers extends Level{
+    public static final String TILEMAP_FILENAME = "Level_theSecretChambers.tmx";
+
+    public TheSecretChambers() { super(TILEMAP_FILENAME); }
+}
