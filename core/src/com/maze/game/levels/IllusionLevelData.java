65cfe85b1d587b9039cca88a7d8e72ada46ff4a9
lucas <Meinhart187>
2022-07-09 13:56:51 +0200

    neue levelbuttons

--- core/src/com/maze/game/levels/IllusionLevelData.java
+++ core/src/com/maze/game/levels/IllusionLevelData.java
@@ -11,8 +11,8 @@ package com.maze.game.levels;
 public class IllusionLevelData extends LevelData{
     private final String name = "Illusion";
     private final String fileName = "illusion.tmx";
-    private final String buttonName = "Bomber_aus.png";
-    private final String buttonPressedName = "bomber_an.png";
+    private final String buttonName = "illusion_aus.png";
+    private final String buttonPressedName = "illusion_an.png";
     // TODO
 
     @Override
